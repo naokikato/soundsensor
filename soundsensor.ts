7028f6631f908dd8644f3c4987f0eb1f9e36fd03
Naoki Kato <naoki@bmoon.jp>
2024-10-10 21:03:16 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ soundsensor.ts
@@ -0,0 +1,19 @@
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#0fbc11 icon="" block="音センサ v1.2"
+namespace IMLsoundsensor {
+
+    //% block
+    //% block="音の大きさ %pin"
+    //% weight=100   
+    export function getLight(pin: AnalogPin): number {
+        let sound = 0;
+        for( let i=0 ; i < 32 ; i++)
+        {
+            sound += pins.analogReadPin(pin)
+        }
+        sound >>= 5
+        return Math.round( sound / 1024.0 * 1000.0) / 10.0
+    }
+}
