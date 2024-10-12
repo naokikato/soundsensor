4f2ef2b5bd23b08176dec3015961ddf1c80a8799
Naoki Kato <naoki@bmoon.jp>
2024-10-12 11:57:42 +0900

    更新 soundsensor.ts

--- soundsensor.ts
+++ soundsensor.ts
@@ -19,4 +19,53 @@ namespace IML_soundsensor {
         sound >>= 5
         return Math.round(sound / 1024.0 * 1000.0) / 10.0
     }
+
+
+
+    let datapin = AnalogPin.P0
+    const SOUND_EVENT_ID1 = 1001;
+    const SOUND_EVENT_ID2 = 1002;
+    let threshold1 = 70;
+    let threshold2 = 40;
+    let interval = 100;
+
+    //% block
+    //% block="%pin につなげた音センサの閾値の上を $value1 下を $value2 に設定する"
+    //% weight=90 color=#3fbc41
+    export function setSoundSensor(pin: AnalogPin, value1: number, value2: number) {
+        datapin = pin
+        threshold1 = value1;
+        threshold2 = value2;
+        startListening();
+    }
+    //% block
+    //% block="音センサの出力が閾値以上になったとき"
+    //% weight=89 color=#3fbc41
+    export function onSoundDetected1(handler: () => void) {
+        control.onEvent(SOUND_EVENT_ID1, EventBusValue.MICROBIT_EVT_ANY, handler);
+    }
+    //% block
+    //% block="音センサの出力が閾値以下になったとき"
+    //% weight=88 color=#3fbc41
+    export function onSoundDetected2(handler: () => void) {
+        control.onEvent(SOUND_EVENT_ID2, EventBusValue.MICROBIT_EVT_ANY, handler);
+    }
+
+    // イベントリスナーの開始
+    function startListening() {
+        control.inBackground(() => {
+            while (true) {
+                let Level = getsound(datapin);
+                if (Level >= threshold1) {
+                    // イベントを発生させる
+                    control.raiseEvent(SOUND_EVENT_ID1, Level);
+                }
+                if (Level <= threshold2) {
+                    // イベントを発生させる
+                    control.raiseEvent(SOUND_EVENT_ID2, Level);
+                }
+                basic.pause(interval);
+            }
+        });
+    }
 }
